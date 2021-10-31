1059b3f1fb2bd0483a8c8332c2d3dfa3c979da6d
Krzysztof Ziembicki <krzysztof.ziembicki@interia.pl>
2021-10-31 12:22:22 +0100

    Lifting to .net core 5

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
@@ -15,34 +14,32 @@ using Microsoft.Extensions.Options;
 
 namespace CoreCodeCamp
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CampContext>();
-            services.AddScoped<ICampRepository, CampRepository>();
+      services.AddDbContext<CampContext>();
+      services.AddScoped<ICampRepository, CampRepository>();
 
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
-            services.AddControllers();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(cfg =>
-            {
-                cfg.MapControllers();
-            });
-        }
+      services.AddControllers();
     }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(cfg =>
+      {
+        cfg.MapControllers();
+      });
+    }
+  }
 }
