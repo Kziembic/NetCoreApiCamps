f59d67331eb53ea555fc58e4e48c76e9db0f2909
Krzysztof Ziembicki <krzysztof.ziembicki@interia.pl>
2021-10-31 18:07:48 +0100

    Searching by query string implemented

--- src/Models/TalkModel.cs
+++ src/Models/TalkModel.cs
@@ -3,6 +3,7 @@ namespace CoreCodeCamp.Data
 {
     public class TalkModel
     {
+        public int TalkId { get; set; }
         public string Title { get; set; }
         public string Abstract { get; set; }
         public int Level { get; set; }
