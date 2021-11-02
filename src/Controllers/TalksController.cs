917d2e4f3f49c410b45efbf367a7416f8207c7f3
Krzysztof Ziembicki <krzysztof.ziembicki@interia.pl>
2021-11-02 18:27:59 +0100

    Added POST method for individual Talk

--- src/Controllers/TalksController.cs
+++ src/Controllers/TalksController.cs
@@ -31,13 +31,75 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
-                var talks = await _repository.GetTalksByMonikerAsync(moniker);
+                var talks = await _repository.GetTalksByMonikerAsync(moniker, true);
 
                 return _mapper.Map<TalkModel[]>(talks);
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talks");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id);
+
+                return _mapper.Map<TalkModel>(talk);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talks");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                if (camp is null)
+                {
+                    return BadRequest("Camp does not exists");
+                }
+
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = camp;
+
+                if (model.Speaker is null)
+                {
+                    return BadRequest("Speaker ID is required");
+                }
+                var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker is null)
+                {
+                    return BadRequest("Speaker could not be found");
+                }
+
+                talk.Speaker = speaker;
+
+                _repository.Add(talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    var url = _linkGenerator.GetPathByAction(HttpContext, 
+                        "Get",
+                        values: new {moniker, id = talk.TalkId });
+
+                    return Created(url, _mapper.Map<TalkModel>(talk));
+                }
+                else
+                {
+                    return BadRequest("Failed to save new Talk");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to post Talk");
             }
         }
     }
