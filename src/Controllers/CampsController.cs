86138a5f5627a428f4060eff2ef868ac5807685d
Krzysztof Ziembicki <krzysztof.ziembicki@interia.pl>
2021-11-05 17:57:13 +0100

    Header versioning added

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
